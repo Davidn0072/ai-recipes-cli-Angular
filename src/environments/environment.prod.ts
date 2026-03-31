bb543f9d7ac286d1255309df0b3e10b6d24ec6e5
Davidn0072 <Davidn0072@Gmail.COM>
2026-03-31 19:32:50 +0300

    Add logging for NG_APP_BUILD_API_URL in production environment: include console log to track API URL during build process for better debugging and verification.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 import type { Environment } from './environment.type';
 import { NG_APP_BUILD_API_URL } from './ng-app-api-url.generated';
 
+console.log('NG_APP_BUILD_API_URL TIME  build:', NG_APP_BUILD_API_URL);
+
 /**
  * `NG_APP_BUILD_API_URL` is written by `scripts/prebuild-env.mjs` from `NG_APP_API_URL`
  * during `npm run build` (Vercel should set `NG_APP_API_URL` for production API base URL).
